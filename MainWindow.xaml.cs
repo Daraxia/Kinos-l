f6bbb473bc6af9bd0235c8f76645fe7a195dc62d
Adéla Homolkova <114067794+Daraxia@users.noreply.github.com>
2024-03-26 20:59:10 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CinemaGridApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateSeatingPlan();
+        }
+
+        private void CreateSeatingPlan()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Button button = new Button
+                {
+                    Background = Brushes.Red,
+                    Width = 30,
+                    Height = 15
+                };
+
+                button.Click += (sender, e) =>
+                {
+                    Button clickedButton = (Button)sender;
+                    if (clickedButton.Background == Brushes.Red)
+                    {
+                        clickedButton.Background = Brushes.Green;
+                    }
+                    else
+                    {
+                        clickedButton.Background = Brushes.Red;
+                    }
+                };
+
+                SeatingPlanGrid.Children.Add(button);
+            }
+        }
+    }
+}
